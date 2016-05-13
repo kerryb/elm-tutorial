39dc909f022a69a0d48d138c1150fb5d2b13b3a7
Kerry Buckley <kerryjbuckley@gmail.com>
2016-05-13 21:42:57 +0100

    Implement reset button

--- 2.1.1-buttons/main.elm
+++ 2.1.1-buttons/main.elm
@@ -2,20 +2,27 @@ import Html exposing (Html, div, text, button)
 import Html.App exposing (beginnerProgram)
 import Html.Events exposing (onClick)
 
-main = beginnerProgram { model = model, view = view, update = update }
+main = beginnerProgram { model = init, view = view, update = update }
 
 type alias Model = {value: Int}
 
 model : Model
-model = {value = 0}
+model = init
 
-type Message = Increment | Decrement
+init : Model
+init = {value = 0}
+
+type Message = Increment | Decrement | Reset
 
 update : Message -> Model -> Model
 update message model =
   case message of
-    Increment -> {model | value = model.value + 1}
-    Decrement -> {model | value = model.value - 1}
+    Increment ->
+      {model | value = model.value + 1}
+    Decrement ->
+      {model | value = model.value - 1}
+    Reset ->
+      init
 
 view : Model -> Html Message
 view model =
@@ -23,4 +30,5 @@ view model =
     [ button [ onClick Decrement ] [ text "-" ]
     , div [] [ text (toString model.value) ]
     , button [ onClick Increment ] [ text "+" ]
+    , button [ onClick Reset ] [ text "Reset" ]
     ]
