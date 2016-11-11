85713ef493c5b7054c5a4b09e0e087ecbd7fd794
Kerry Buckley <kerryjbuckley@gmail.com>
2016-11-11 21:42:57 +0000

    Reversing text field

---
+++ user-input/text-fields/main.elm
@@ -0,0 +1,31 @@
+import Html exposing (Html, Attribute, div, input, text)
+import Html.App as App
+import Html.Attributes exposing (placeholder)
+import Html.Events exposing (onInput)
+import String
+
+main = App.beginnerProgram { model = init, view = view, update = update }
+
+-- MODEL
+
+type alias Model = { content : String }
+init : Model
+init = { content = "" }
+
+-- UPDATE
+
+type Message = Change String
+
+update : Message -> Model -> Model
+update message model =
+  case message of
+    Change newContent -> { model | content = newContent }
+
+-- VIEW
+
+view : Model -> Html Message
+view model =
+  div []
+    [ input [ placeholder "Text to reverse", onInput Change ] []
+    , div [] [text (String.reverse model.content) ]
+    ]
